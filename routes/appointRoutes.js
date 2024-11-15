7eb1a98e7f6442c5f513568703e3d43420228c5a
Padmaja <134283145+hapadmaja@users.noreply.github.com>
2024-11-15 17:07:59 +0530

    Update appointRoutes.js

--- routes/appointRoutes.js
+++ routes/appointRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const appointmentController = require("../controllers/appointmentController");
-
 const appointRouter = express.Router();
 
 appointRouter.get(
